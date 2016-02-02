0f9d3f689f96c897ccb0ba40bb7891faabb69d22
Henry Yao <henry.yao@airbnb.com>
2016-02-02 14:38:35 -0800

    config

--- mobile/data/config.php
+++ mobile/data/config.php
@@ -1,11 +1,1 @@
 <?php
-return array(
-    'DB_HOST'   => 'hdm19160134.my3w.com',
-    'DB_USER'   => 'hdm19160134',
-    'DB_PWD'   => 'wawacang123',
-    'DB_NAME'   => 'hdm19160134_db',
-    'DB_PREFIX'   => 'wwc_',
-    'DB_PORT'   => '3306',
-    'DB_CHARSET'   => 'utf8',
-    'DB_TYPE'   => 'mysql',
-);
